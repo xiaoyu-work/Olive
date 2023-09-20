82f32554f2a2c00eeebfc80863f65534b94cd749
trajepl <jiapli@microsoft.com>
2023-09-20 21:05:20 +0800

    test2

--- test/unit_test/passes/onnx/test_model_optimizer.py
+++ test/unit_test/passes/onnx/test_model_optimizer.py
@@ -6,6 +6,7 @@ from test.unit_test.utils import get_onnx_model
 
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx import OnnxModelOptimizer
+import pytest
 
 
 def test_onnx_model_optimizer_pass(tmp_path):
@@ -16,3 +17,30 @@ def test_onnx_model_optimizer_pass(tmp_path):
 
     # execute
     p.run(input_model, None, output_folder)
+
+
+@pytest.mark.parametrize("use_sess", [True, False])
+def test_onnx_model_temp_path(use_sess):
+    from pathlib import Path
+    import tempfile
+    import onnx
+    import onnxruntime
+    from optimum.exporters.onnx import main_export
+    curr_path = Path(__file__).parent
+    main_export(
+        "distilbert-base-uncased-finetuned-sst-2-english",
+        output=str(curr_path)
+    )
+    input_model = onnx.load(str(curr_path / "model.onnx"))
+    with tempfile.TemporaryDirectory(prefix="pre-quant") as temp_dir:
+        temp_path = Path(temp_dir)
+        output_model_path_1 = temp_path / "input_model1.onnx"
+        onnx.save_model(input_model, str(output_model_path_1), save_as_external_data=True)
+        if use_sess:
+            sess_option = onnxruntime.SessionOptions()
+            sess_option.optimized_model_filepath = str(temp_path / "optimized.onnx")
+            sess_option.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_BASIC
+            _ = onnxruntime.InferenceSession(output_model_path_1, sess_option, providers=["CPUExecutionProvider"])
+        else:
+            ...
+    print("test_onnx_model_temp_path done")
